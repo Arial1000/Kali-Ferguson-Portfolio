5eac2872de8c548d5427f1ac12e01941d4cafb2b
Arial1000 <lyricthefirst@gmail.com>
2025-07-08 16:30:39 -0400

    changed name

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,8 +16,8 @@ function Main() {
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Kali Ferguson</h1>
+          <p>Computer Science Graduate</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
