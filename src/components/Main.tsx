9f11e0ad1cbefb8baa8f52403b40f842e0451d2d
Arial1000 <lyricthefirst@gmail.com>
2025-07-08 16:47:34 -0400

    inserted avatar and cropped

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
+import headshot from '../assets/images/Headshot.jpg';
+
 
 function Main() {
 
@@ -9,7 +11,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={headshot} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
